32840b61932256df3750203a9e297f6d64245ace
Bielzinzx717 <gabrielborges0111@gmail.com>
2025-11-07 13:30:22 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, 
 from reportlab.lib.units import inch
 from flask_wtf.csrf import CSRFProtect
 import re
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-key')
@@ -21,6 +23,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://usuario:senha@host/banc
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['WTF_CSRF_ENABLED'] = True
 app.config['WTF_CSRF_TIME_LIMIT'] = None
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 db.init_app(app)
 login_manager.init_app(app)
@@ -665,6 +673,7 @@ def export_pdf():
 
 if __name__ == '__main__':
     app.run(debug=False)
+
 
 
 
